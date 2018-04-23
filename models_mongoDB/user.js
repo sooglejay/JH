68a8e7dd635748960ebbc139fa678f6527702340
johnny-jiangli <jiangli450324@163.com>
2018-04-23 15:59:41 +0800

    MongoDB连接测试与文档插入

--- models_mongoDB/user.js
+++ models_mongoDB/user.js
@@ -1,9 +1,25 @@
+//创建集合
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/runoob";
-
 MongoClient.connect(url,function (err,db) {
-   if (err) throw err;
-   console.log("数据库连接!");
-   db.close();
+    if(err) throw err;
+    console.log("数据库连接！");
+    var dbase = db.db("runoob");
+    dbase.createCollection('user',function (err,res) {
+       if(err) throw err;
+       console.log("创建集合!");
+        /**
+         * 如果插入多条，可以用insertMany方法 ,更新一条活着多条：updateOne Or updateMany两个方法,
+         * 更多操作请登录www:mongodb.org查询api
+         *
+         */
+
+       dbase.collection("user").insertOne({"name":"奖励"},function (err,res) {
+           if(err) throw err;
+           console.log("文档插入成功!");
+           // var obj = dbase.collection('user').findOne;
+           // console.log(obj);
+       });
+       db.close();
+    });
 });
-s
