e2613526c51223fa5c48b8197162a6ac728a0c90
johnny-jiangli <jiangli450324@163.com>
2018-04-24 13:20:48 +0800

    Assert模块测试使用与MongoDB的增删改查

--- models_mongoDB/mongoDBTest.js
+++ models_mongoDB/mongoDBTest.js
@@ -1,8 +1,46 @@
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/runoob";
+// var MongoClient = require("mongodb").MongoClient;
+// var url = "mongodb://localhost:27017/runoob";
+//
+// MongoClient.connect(url,function (err,db) {
+//     if (err) throw err;
+//     console.log("数据库连接!");
+//     db.close();
+// });
 
-MongoClient.connect(url,function (err,db) {
-    if (err) throw err;
-    console.log("数据库连接!");
-    db.close();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require("assert");
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'myproject';
+
+MongoClient.connect(url,function (err,client) {
+    assert.equal(null,err);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+
+    insertDocuments(db,function (res) {
+        console.log(res.ops);
+        client.close()
+    })
 });
+/**
+ * 插入文档，
+ * @param err
+ * @param callback 传入一个result，这里面包含了对插入文档的结果
+ */
+const insertDocuments = function (err,callback) {
+    const collection = db.collection('documents');
+    collection.insertMany(
+        [
+            {a:1},{a:2},{a:3}
+        ]
+    ,function (err,result) {
+            assert.equal(err,null);
+            assert.equal(3,result.result.n);
+            assert.equal(3,result.ops.length);
+            console.log("Inserted 3 documents into the collection");
+            callback(result);
+        });
+}
+
