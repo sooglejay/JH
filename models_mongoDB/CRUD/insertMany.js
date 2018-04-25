8be63e42f66690cfdc905129b2130b45eccfc646
johnny-jiangli <jiangli450324@163.com>
2018-04-25 23:10:52 +0800

    使用insertMany方法插入文档时还指定了数字数据类型

---
+++ models_mongoDB/CRUD/insertMany.js
@@ -0,0 +1,30 @@
+/*
+使用insertMany方法插入文档时还指定了数字数据类型
+ */
+const Long = require("mongodb").Long;
+const Decimal = require("mongodb").Decimal128;
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = 'mongodb://localhost:27107';
+
+const dbName = 'myproject';
+
+MongoClient.connect(url,function (err,client) {
+    assert.equal(null,err);
+    console.log("Connected correctly to server!");
+
+    const db = client.db(dbName);
+    const longValue =Long(1787);
+    const decimalValue = Decimal.fromString("27.8892836");
+
+    db.collection('user').insertMany([
+        {a:longValue},
+        {b:decimalValue}
+    ],function (err,result) {
+        assert.equal(null,err);
+        assert.equal(2,result.insertedCount);
+        client.close()
+    })
+})
