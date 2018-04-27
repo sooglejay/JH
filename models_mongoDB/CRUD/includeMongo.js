37a932b192a1165a60e9c64eb07d28f61a2a3687
johnny-jiangli <jiangli450324@163.com>
2018-04-27 16:29:52 +0800

    - -!这是一个失败的测试代码

---
+++ models_mongoDB/CRUD/includeMongo.js
@@ -0,0 +1,6 @@
+    //
+    // exports.MongoClient = require("mongodb").MongoClient;
+    // exports.assert = require("assert");
+    //
+    // exports.url = "mongodb://127.0.0.1:27017";
+    // exports.dbName = 'user';
