67575f11979e66ba4a69b2b98bced3d74a8f9003
johnny-jiangli <jiangli450324@163.com>
2018-04-27 15:50:11 +0800

    执行批量的API操作

---
+++ models_mongoDB/CRUD/bulkWrlte.js
@@ -0,0 +1,22 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://127.0.0.1:27017";
+const dbName = 'user';
+
+MongoClient.connect(url,function (err,client) {
+    assert.equal(null,err);
+    const db = client.db(dbName);
+    const col = db.collection('col');
+    /**
+     * 批量执行api
+     */
+    col.bulkWrite([
+        {insertOne:{document:{a:1}}},
+        {updateOne:{filter:{a:2},update:{$set:{a:2}},upsert:true}}
+    ],{ordered:true,w:1},function (bulkWriteError,result) {
+        assert.equal(null,err);
+        assert.equal(1,result.insertedCount);
+        assert.equal(2,r)
+    })
+})
