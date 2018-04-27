1479a21b7136c5f7c10f9ce211df67fa66d8e44a
johnny-jiangli <jiangli450324@163.com>
2018-04-27 15:52:44 +0800

    执行批量的API操作 以对象的形式传入对应的API，接受第二个参数对象形式的，里面包含了一些对执行内部的排序读写权限以及其他

--- models_mongoDB/CRUD/bulkWrlte.js
+++ models_mongoDB/CRUD/bulkWrlte.js
@@ -15,7 +15,7 @@ MongoClient.connect(url,function (err,client) {
         {insertOne:{document:{a:1}}},
         {updateOne:{filter:{a:2},update:{$set:{a:2}},upsert:true}}
     ],{ordered:true,w:1},function (bulkWriteError,result) {
-        assert.equal(null,err);
+        assert.equal(null,bulkWriteError);
         assert.equal(1,result.insertedCount);
         assert.equal(2,r)
     })
