e1e2300732b5b51d5c0f6d5414325680397ffaf5
johnny-jiangli <jiangli450324@163.com>
2018-04-29 22:23:13 +0800

    模块mongoose

--- mongooseDemoConnect/connectMongo.js
+++ mongooseDemoConnect/connectMongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/runoob');
+mongoose.connect('mongodb://localhost/runoob');
 
 var db = mongoose.connection;
 
--- mongooseDemoConnect/fristDemo.js
+++ mongooseDemoConnect/fristDemo.js
@@ -1,48 +1,61 @@
-// const mongoose = require("mongoose");
-// mongoose.connect('mongddb:localhost:217017/user');
-// const db = mongoose.connection;
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/user');
+const db = mongoose.connection;
+
+db.on("error",console.error.bind(console,'connection error'));
+
+db.once("open",function(){
+    console.log("hello")
+});
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/user');
 //
-// db.on("error",console.error.bind(console,'connection error'));
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function(err,client) {
+//     // we're connected!
+//     // console.log("hello");
+//     console.log(client);
+//     console.log("成功连接到测试数据库！")
+// });
 //
-// db.once("open",function(){
-//     console.log("hello")
+// var KittySchema= mongoose.Schema({name:String});
+// KittySchema.methods.speak = function () {
+//     var greeting = this.name
+//     ?"Meow name is "+this.name
+//         :"I don't have a name";
+//     console.log(greeting);
+// };
+// KittySchema.methods.hello = function () {
+//     console.log("hello"+this.name);
+// };
+//
+// var Kitten = mongoose.model('Kitten',KittySchema);
+//
+// var fluffy =  new Kitten({name:'flffy'});
+// fluffy.speak();
+// fluffy.hello();
+//
+// fluffy.save(function(err,fluffy){
+//     if(err) return console.error(err);
+//     fluffy.speak();
+// });
+//
+// var silence = new Kitten({name:'Silence'});
+// silence.speak();
+// silence.hello();
+// silence.save();
+// // console.log(silence.name);
+//
+// Kitten.find(function (err,kittens) {
+//     if(err)return console.log(err);
+//     console.log(kittens);
 // });
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/user');
+const schema = mongoose.Schema({name:String});
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(err,client) {
-    // we're connected!
-    console.log("hello");
-    console.log(client);
-    console.log("成功连接到测试数据库！")
-});
+const col = mongoose.model('col',schema);
 
-var KittySchema= mongoose.Schema({name:String});
-KittySchema.methods.speak = function () {
-    var greeting = this.name
-    ?"Meow name is "+this.name
-        :"I don't have a name";
-    console.log(greeting);
-};
-
-var Kitten = mongoose.model('Kitten',KittySchema);
-
-var fluffy =  new Kitten({name:'flffy'});
-fluffy.speak();
-
-fluffy.save(function(err,fluffy){
-    if(err) return console.error(err);
-    fluffy.speak();
-});
-
-var silence = new Kitten({name:'Silence'});
-silence.save();
-// console.log(silence.name);
-
-Kitten.find(function (err,kittens) {
-    if(err)return console.log(err);
-    console.log(kittens);
-});
+const doc = new col({name:"jiangli"});
+console.log(doc.name)
