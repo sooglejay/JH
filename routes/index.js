ff967b8fa5ee0d8ec5fc2b0019325b1e59c22735
johnny-jiangli <jiangli450324@163.com>
2018-05-05 17:26:28 +0800

    添加电影分页的路由

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
-var jhbolg = require('../mongooseDemoConnect/JHmodel/JHSchema');
+// var jhbolg = require('../mongooseDemoConnect/JHmodel/JHSchema');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  jhbolg.fetch(function (err,bolg) {
-     if(err) console.log(err);
+  // jhbolg.fetch(function (err,bolg) {
+  //    if(err) console.log(err);
 
      res.render('index', {
          title: 'JH博客网',
-         bolg:bolg
+         bolg:{title:"jh",doc:"这是test"}
      });
-  });
+  // });
 
 });
-router.get('/bolg/:id',function (req,res) {
-
+router.get('/movie/:id',function(req,res){
+    res.render('detail',{title:"详情页"})
 });
+
+router.get('/admin/list',function (req,res) {
+    res.render('list',{title:"这是list"})
+});
+
+router.get('/admin/movie',function (req,res) {
+   res.render('admin',{title:"这是admin"})
+});
+
 module.exports = router;
